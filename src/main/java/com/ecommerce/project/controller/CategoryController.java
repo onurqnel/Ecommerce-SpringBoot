77dde9e9e3e3126c31cbc1a0599a8421686ddef1
onurqnel <onur.onel2230@gmail.com>
2024-09-26 17:49:18 -0400

    Created Category Get and Post methods

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -2,6 +2,8 @@ package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -13,6 +15,12 @@ public class CategoryController {
 
     @GetMapping("/api/public/categories")
     public List<Category> getAllCategories() {
-     return categories;
+        return categories;
+    }
+
+    @PostMapping("/api/public/categories")
+    public String createCategory(@RequestBody Category category) {
+        categories.add(category);
+        return "Category added successfully!";
     }
 }
